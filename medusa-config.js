6f1a50e22f75da529b2539cbbf667c190d7c3232
Ricardo <roicoroy@yahoo.com.br>
2023-04-12 14:26:42 +0100

    init

---
+++ medusa-config.js
@@ -0,0 +1,117 @@
+const dotenv = require("dotenv");
+
+let ENV_FILE_NAME = "";
+switch (process.env.NODE_ENV) {
+  case "production":
+    ENV_FILE_NAME = ".env.production";
+    break;
+  case "staging":
+    ENV_FILE_NAME = ".env.staging";
+    break;
+  case "test":
+    ENV_FILE_NAME = ".env.test";
+    break;
+  case "development":
+  default:
+    ENV_FILE_NAME = ".env";
+    break;
+}
+
+try {
+  dotenv.config({ path: process.cwd() + "/" + ENV_FILE_NAME });
+} catch (e) {}
+
+// CORS when consuming Medusa from admin
+const ADMIN_CORS =
+  process.env.ADMIN_CORS;
+
+// CORS to avoid issues when consuming Medusa from a client
+const STORE_CORS = process.env.STORE_CORS;
+
+const DATABASE_TYPE = process.env.DATABASE_TYPE;
+const DATABASE_URL = process.env.DATABASE_URL;
+// Medusa uses Redis, so this needs configuration as well
+const REDIS_URL = process.env.REDIS_URL;
+
+// Stripe keys
+const STRIPE_API_KEY = process.env.STRIPE_API_KEY;
+const STRIPE_WEBHOOK_SECRET = process.env.STRIPE_WEBHOOK_SECRET;
+
+const plugins = [
+  `medusa-fulfillment-manual`,
+  `medusa-payment-manual`,
+  // To enable the admin plugin, uncomment the following lines and run `yarn add @medusajs/admin`
+  {
+    resolve: "@medusajs/admin",
+    /** @type {import('@medusajs/admin').PluginOptions} */
+    options: {
+      autoRebuild: true,
+    },
+  },
+  {
+    resolve: `medusa-payment-stripe`,
+    options: {
+      api_key: STRIPE_API_KEY,
+      webhook_secret: STRIPE_WEBHOOK_SECRET,
+    },
+  },
+  {
+    resolve: `medusa-file-cloudinary`,
+    options: {
+      cloud_name: process.env.CLOUDINARY_NAME,
+      api_key: process.env.CLOUDINARY_KEY,
+      api_secret: process.env.CLOUDINARY_SECRET,
+      secure: true,
+    },
+  },
+    // {
+  //   resolve: `medusa-plugin-strapi`,
+  //   options: {
+  //     strapi_medusa_user: process.env.STRAPI_USER,
+  //     strapi_medusa_password: process.env.STRAPI_PASSWORD,
+  //     strapi_url: process.env.STRAPI_URL,
+  //     strapi_port: process.env.STRAPI_PORT,
+  //     strapi_protocol: process.env.STRAPI_PROTOCOL
+  //   }
+  // },
+];
+
+const modules = {
+  /*eventBus: {
+    resolve: "@medusajs/event-bus-redis",
+    options: {
+      redisUrl: REDIS_URL
+    }
+  },
+  cacheService: {
+    resolve: "@medusajs/cache-redis",
+    options: {
+      redisUrl: REDIS_URL
+    }
+  },*/
+}
+
+/** @type {import('@medusajs/medusa').ConfigModule["projectConfig"]} */
+const projectConfig = {
+  jwtSecret: process.env.JWT_SECRET,
+  cookieSecret: process.env.COOKIE_SECRET,
+  database_database: "./medusa-db.sql",
+  database_type: DATABASE_TYPE,
+  store_cors: STORE_CORS,
+  admin_cors: ADMIN_CORS,
+  // Uncomment the following lines to enable REDIS
+  // redis_url = REDIS_URL
+}
+
+if (DATABASE_URL && DATABASE_TYPE === "postgres") {
+  projectConfig.database_url = DATABASE_URL;
+  delete projectConfig["database_database"];
+}
+
+
+/** @type {import('@medusajs/medusa').ConfigModule} */
+module.exports = {
+  projectConfig,
+  plugins,
+	modules,
+};

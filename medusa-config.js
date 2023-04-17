1cbf7e57e4f8dc33fe62ce3e67dbafcb59bc72bb
Ricardo <roicoroy@yahoo.com.br>
2023-04-17 10:24:34 +0100

    admin

--- medusa-config.js
+++ medusa-config.js
@@ -46,7 +46,7 @@ const plugins = [
     resolve: "@medusajs/admin",
     /** @type {import('@medusajs/admin').PluginOptions} */
     options: {
-      autoRebuild: true,
+      autoRebuild: false,
     },
   },
   {

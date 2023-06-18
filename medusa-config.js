6c252800603abe42aa998e618234a8b22b0ae952
Ricardo Watanabe Bento <roicoroy@yahoo.com.br>
2023-06-18 13:09:39 +0100

    db type

--- medusa-config.js
+++ medusa-config.js
@@ -74,6 +74,13 @@ const projectConfig = {
   store_cors: STORE_CORS,
   database_url: DATABASE_URL,
   admin_cors: ADMIN_CORS,
+  database_type: 'postgres',
+  database_schema:'public',
+  database_extra: {
+      ssl: {
+          rejectUnauthorized: false
+      }
+  },
   // Uncomment the following lines to enable REDIS
   redis_url: REDIS_URL
 };

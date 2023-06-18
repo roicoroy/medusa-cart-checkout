0c26cf422c8b395f3c35579239558d2349bc12ee
Ricardo Watanabe Bento <roicoroy@yahoo.com.br>
2023-06-18 16:50:02 +0100

    local

--- medusa-config.js
+++ medusa-config.js
@@ -1,68 +1,49 @@
-// const dotenv = require("dotenv");
-
-// let ENV_FILE_NAME = "";
-// switch (process.env.NODE_ENV) {
-//   case "production":
-//     ENV_FILE_NAME = ".env.production";
-//     break;
-//   case "staging":
-//     ENV_FILE_NAME = ".env.staging";
-//     break;
-//   case "test":
-//     ENV_FILE_NAME = ".env.test";
-//     break;
-//   case "development":
-//   default:
-//     ENV_FILE_NAME = ".env";
-//     break;
-// }
-
-// try {
-//   dotenv.config({ path: process.cwd() + "/" + ENV_FILE_NAME });
-// } catch (e) { }
-
-// CORS when consuming Medusa from admin
-// const ADMIN_CORS =
-//   process.env.ADMIN_CORS;
-
-// // CORS to avoid issues when consuming Medusa from a client
-// const STORE_CORS = process.env.STORE_CORS;
-
-// const DATABASE_URL =
-//   process.env.DATABASE_URL;
-
-// const REDIS_URL = process.env.REDIS_URL;
-
 const plugins = [
   `medusa-fulfillment-manual`,
   `medusa-payment-manual`,
-  {
-    resolve: `@medusajs/file-local`,
-    options: {
-      upload_dir: "uploads",
-    },
-  },
-  // To enable the admin plugin, uncomment the following lines and run `yarn add @medusajs/admin`.
   // {
-  //   resolve: "@medusajs/admin",
-  //   /** @type {import('@medusajs/admin').PluginOptions} */
+  //   resolve: `@medusajs/file-local`,
   //   options: {
-  //     autoRebuild: true,
+  //     upload_dir: "uploads",
   //   },
   // },
+  // To enable the admin plugin, uncomment the following lines and run `yarn add @medusajs/admin`.
+  {
+    resolve: "@medusajs/admin",
+    /** @type {import('@medusajs/admin').PluginOptions} */
+    options: {
+      autoRebuild: true,
+    },
+  },
+  {
+    resolve: `medusa-payment-stripe`,
+    options: {
+      api_key: process.env.STRIPE_API_KEY,
+      webhook_secret: process.env.STRIPE_WEBHOOK_SECRET,
+    },
+  },
+  {
+    resolve: `medusa-file-cloudinary`,
+    options: {
+      cloud_name: process.env.CLOUDINARY_NAME,
+      api_key: process.env.CLOUDINARY_KEY,
+      api_secret: process.env.CLOUDINARY_SECRET,
+      secure: true,
+    },
+  },
 ];
 
 const modules = {
   // eventBus: {
   //   resolve: "@medusajs/event-bus-redis",
   //   options: {
-  //     redisUrl: REDIS_URL
+  //     redisUrl: process.env.REDIS_URL
   //   }
   // },
   // cacheService: {
   //   resolve: "@medusajs/cache-redis",
   //   options: {
-  //     redisUrl: REDIS_URL
+  //     redisUrl: process.env.REDIS_URL
   //   }
   // },
 };

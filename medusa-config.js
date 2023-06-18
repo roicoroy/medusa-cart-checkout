2409fc04f0950fdbb64603d28ef5b7496a2479ee
Ricardo Watanabe Bento <roicoroy@yahoo.com.br>
2023-06-18 17:04:42 +0100

    admin update

--- medusa-config.js
+++ medusa-config.js
@@ -8,13 +8,14 @@ const plugins = [
   //   },
   // },
   // To enable the admin plugin, uncomment the following lines and run `yarn add @medusajs/admin`.
-  // {
-  //   resolve: "@medusajs/admin",
-  //   /** @type {import('@medusajs/admin').PluginOptions} */
-  //   options: {
-  //     autoRebuild: true,
-  //   },
-  // },
+  {
+    resolve: "@medusajs/admin",
+    /** @type {import('@medusajs/admin').PluginOptions} */
+    options: {
+      autoRebuild: false,
+      serve: true
+    },
+  },
   {
     resolve: `medusa-payment-stripe`,
     options: {

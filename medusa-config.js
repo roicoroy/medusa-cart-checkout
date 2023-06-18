c62285719f560099d54c97b0819dc140dfc91125
Ricardo Watanabe Bento <roicoroy@yahoo.com.br>
2023-06-18 15:33:44 +0100

    env

--- medusa-config.js
+++ medusa-config.js
@@ -1,37 +1,37 @@
-const dotenv = require("dotenv");
+// const dotenv = require("dotenv");
 
-let ENV_FILE_NAME = "";
-switch (process.env.NODE_ENV) {
-  case "production":
-    ENV_FILE_NAME = ".env.production";
-    break;
-  case "staging":
-    ENV_FILE_NAME = ".env.staging";
-    break;
-  case "test":
-    ENV_FILE_NAME = ".env.test";
-    break;
-  case "development":
-  default:
-    ENV_FILE_NAME = ".env";
-    break;
-}
+// let ENV_FILE_NAME = "";
+// switch (process.env.NODE_ENV) {
+//   case "production":
+//     ENV_FILE_NAME = ".env.production";
+//     break;
+//   case "staging":
+//     ENV_FILE_NAME = ".env.staging";
+//     break;
+//   case "test":
+//     ENV_FILE_NAME = ".env.test";
+//     break;
+//   case "development":
+//   default:
+//     ENV_FILE_NAME = ".env";
+//     break;
+// }
 
-try {
-  dotenv.config({ path: process.cwd() + "/" + ENV_FILE_NAME });
-} catch (e) { }
+// try {
+//   dotenv.config({ path: process.cwd() + "/" + ENV_FILE_NAME });
+// } catch (e) { }
 
 // CORS when consuming Medusa from admin
-const ADMIN_CORS =
-  process.env.ADMIN_CORS;
+// const ADMIN_CORS =
+//   process.env.ADMIN_CORS;
 
-// CORS to avoid issues when consuming Medusa from a client
-const STORE_CORS = process.env.STORE_CORS;
+// // CORS to avoid issues when consuming Medusa from a client
+// const STORE_CORS = process.env.STORE_CORS;
 
-const DATABASE_URL =
-  process.env.DATABASE_URL;
+// const DATABASE_URL =
+//   process.env.DATABASE_URL;
 
-const REDIS_URL = process.env.REDIS_URL;
+// const REDIS_URL = process.env.REDIS_URL;
 
 const plugins = [
   `medusa-fulfillment-manual`,
@@ -69,11 +69,11 @@ const modules = {
 
 /** @type {import('@medusajs/medusa').ConfigModule["projectConfig"]} */
 const projectConfig = {
-  redis_url: REDIS_URL,
-  database_url: DATABASE_URL,
+  redis_url: process.env.REDIS_URL,
+  database_url: process.env.DATABASE_URL,
   database_type: "postgres",
-  store_cors: STORE_CORS,
-  admin_cors: ADMIN_CORS,
+  store_cors: process.env.STORE_CORS,
+  admin_cors: process.env.ADMIN_CORS,
   // database_extra:
   //   process.env.NODE_ENV !== "development"
   //     ? { ssl: { rejectUnauthorized: false } }

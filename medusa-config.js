f8129db7426331c236b5f9bb144fccdad9239fa9
Ricardo Watanabe Bento <roicoroy@yahoo.com.br>
2023-06-18 09:07:35 +0100

    meudsa updated

--- medusa-config.js
+++ medusa-config.js
@@ -1,25 +1,25 @@
-// const dotenv = require("dotenv");
+const dotenv = require("dotenv");
 
-// let ENV_FILE_NAME = "";
-// switch (process.env.NODE_ENV) {
-//   case "production":
-//     ENV_FILE_NAME = ".env.production";
-//     break;
-//   case "staging":
-//     ENV_FILE_NAME = ".env.staging";
-//     break;
-//   case "test":
-//     ENV_FILE_NAME = ".env.test";
-//     break;
-//   case "development":
-//   default:
-//     ENV_FILE_NAME = ".env";
-//     break;
-// }
+let ENV_FILE_NAME = "";
+switch (process.env.NODE_ENV) {
+  case "production":
+    ENV_FILE_NAME = ".env.production";
+    break;
+  case "staging":
+    ENV_FILE_NAME = ".env.staging";
+    break;
+  case "test":
+    ENV_FILE_NAME = ".env.test";
+    break;
+  case "development":
+  default:
+    ENV_FILE_NAME = ".env";
+    break;
+}
 
-// try {
-//   dotenv.config({ path: process.cwd() + "/" + ENV_FILE_NAME });
-// } catch (e) { }
+try {
+  dotenv.config({ path: process.cwd() + "/" + ENV_FILE_NAME });
+} catch (e) {}
 
 // CORS when consuming Medusa from admin
 const ADMIN_CORS =
@@ -28,85 +28,59 @@ const ADMIN_CORS =
 // CORS to avoid issues when consuming Medusa from a client
 const STORE_CORS = process.env.STORE_CORS;
 
-const DATABASE_TYPE = process.env.DATABASE_TYPE;
-const DATABASE_URL = process.env.DATABASE_URL;
+const DATABASE_URL =
+  process.env.DATABASE_URL;
+
 const REDIS_URL = process.env.REDIS_URL;
 
 const plugins = [
   `medusa-fulfillment-manual`,
   `medusa-payment-manual`,
+  {
+    resolve: `@medusajs/file-local`,
+    options: {
+      upload_dir: "uploads",
+    },
+  },
   // To enable the admin plugin, uncomment the following lines and run `yarn add @medusajs/admin`
-  {
-    resolve: "@medusajs/admin",
-    /** @type {import('@medusajs/admin').PluginOptions} */
-    options: {
-      autoRebuild: true,
-    },
-  },
-  {
-    resolve: `medusa-payment-stripe`,
-    options: {
-      api_key: process.env.STRIPE_API_KEY,
-      webhook_secret: process.env.STRIPE_WEBHOOK_SECRET,
-    },
-  },
-  {
-    resolve: `medusa-file-cloudinary`,
-    options: {
-      cloud_name: process.env.CLOUDINARY_NAME,
-      api_key: process.env.CLOUDINARY_KEY,
-      api_secret: process.env.CLOUDINARY_SECRET,
-      secure: true,
-    },
-  },
+  // {
+  //   resolve: "@medusajs/admin",
+  //   /** @type {import('@medusajs/admin').PluginOptions} */
+  //   options: {
+  //     autoRebuild: true,
+  //   },
+  // },
 ];
 
-// const modules = {
-//   // eventBus: {
-//   //   resolve: "@medusajs/event-bus-redis",
-//   //   options: {
-//   //     redisUrl: REDIS_URL
-//   //   }
-//   // },
-//   // cacheService: {
-//   //   resolve: "@medusajs/cache-redis",
-//   //   options: {
-//   //     redisUrl: REDIS_URL
-//   //   }
-//   // },
-// }
+const modules = {
+  /*eventBus: {
+    resolve: "@medusajs/event-bus-redis",
+    options: {
+      redisUrl: REDIS_URL
+    }
+  },
+  cacheService: {
+    resolve: "@medusajs/cache-redis",
+    options: {
+      redisUrl: REDIS_URL
+    }
+  },*/
+};
 
-// /** @type {import('@medusajs/medusa').ConfigModule["projectConfig"]} */
-// const projectConfig = {
-  
-//   // jwtSecret: process.env.JWT_SECRET,
-//   // cookieSecret: process.env.COOKIE_SECRET,
-//   // database_database: "./medusa-db.sql",
-//   // database_type: DATABASE_TYPE,
-//   // store_cors: STORE_CORS,
-//   // admin_cors: ADMIN_CORS,
-//   // // redis_url: REDIS_URL
-// }
-
-// // if (DATABASE_URL && DATABASE_TYPE === "postgres") {
-// //   projectConfig.database_url = DATABASE_URL;
-// //   delete projectConfig["database_database"];
-// // }
-
+/** @type {import('@medusajs/medusa').ConfigModule["projectConfig"]} */
+const projectConfig = {
+  jwtSecret: process.env.JWT_SECRET,
+  cookieSecret: process.env.COOKIE_SECRET,
+  store_cors: STORE_CORS,
+  database_url: DATABASE_URL,
+  admin_cors: ADMIN_CORS,
+  // Uncomment the following lines to enable REDIS
+  redis_url: REDIS_URL
+};
 
 /** @type {import('@medusajs/medusa').ConfigModule} */
-// module.exports = {
-//   projectConfig,
-//   plugins,
-//   // modules,
-// };
 module.exports = {
-  projectConfig: {
-    // redis_url: REDIS_URL,
-    database_url: DATABASE_URL,
-    database_type: "postgres",
-    store_cors: STORE_CORS,
-    admin_cors: ADMIN_CORS,
-  },
+  projectConfig,
   plugins,
+  modules,
 };
